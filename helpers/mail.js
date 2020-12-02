062827b5e5529ce037ae5b937d073e6682c70779
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-12-02 15:59:21 +0100

    added mail helper

---
+++ helpers/mail.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+//const confirmation = require("./mailTemplates/confirmation");
+
+const config = {
+  user: "chronicwatchza@gmail.com",
+  pass: "AFSA_Health_Department+27",
+};
+/** send email method
+ *
+ * @param {*} receiver receiver email
+ * @param {*} valueObjects values to transfer to view files
+ * @param {*} fileName name of email template file
+ * @param {*} handleResult  callback function with error ,info
+ *  object (info.response) as parameter respectively
+ */
+exports.sendMail = (receiver, valueObjects, fileName, handleResult) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: config.user,
+      pass: config.pass,
+    },
+  });
+  const confirmation = require("./mailTemplates/" + fileName);
+
+  const mailOptions = {
+    from: config.user,
+    to: receiver,
+    subject: valueObjects.title,
+    html: confirmation.confirmation(valueObjects),
+  };
+
+  transporter.sendMail(mailOptions, handleResult);
+};
---
+++ helpers/mailTemplates/confirmation.js
@@ -0,0 +1,16 @@
+exports.confirmation = (data) => {
+  return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${data.title}</title>
+</head>
+<body>
+    this is the value = ${data.name}
+</body>
+</html>
+    
+    `;
+};
