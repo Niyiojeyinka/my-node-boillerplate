106438fffd78dad5fb030ca6b6aaf42234b63986
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-12-05 08:34:52 +0100

    added multer middleware

---
+++ middlewares/multer-conf.js
@@ -0,0 +1,20 @@
+const multer = require("multer");
+
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+  "image/gif": "gif",
+};
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "assets/uploads");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+module.exports = multer({ storage: storage }).single("image");
