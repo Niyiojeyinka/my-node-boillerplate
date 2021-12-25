c3fe47f50df0f06e97b8ce8a3efcc8ff92951468
niyiojeyinka <olaniyiojeyinka@gmail.com>
2021-12-25 19:37:48 +0100

    wip better error handlin

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,58 +1,50 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
-exports.login = (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res
-          .status(401)
-          .json({ result: 0, error: new Error("User Not Found!") });
-      }
-      bcrypt.compare(req.body.password, user.password).then((valid) => {
-        if (!valid) {
-          return res
-            .status(401)
-            .json({ result: 0, error: new Error("Incorrect Password!") });
-        }
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
-        const token = jwt.sign({ userId: user._id }, "RANDOM_KEY", {
-          expiresIn: "24h",
-        });
-        return res.status(200).json({
-          result: 1,
-          data: {
-            userId: user._id,
-            token: token,
-          },
-        });
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json({ result: 0, error: error });
+    if (!user) {
+      throw "Error";
+    }
+
+    const valid = await bcrypt.compare(req.body.password, user.password);
+
+    if (!valid) {
+      throw "Not valid";
+    }
+
+    const token = jwt.sign({ userId: user._id }, "RANDOM_KEY", {
+      expiresIn: "24h",
     });
+
+    return res.status(200).json({
+      data: {
+        userId: user._id,
+        token: token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.register = (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = new User({
+exports.register = async (req, res, next) => {
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10);
+
+    const user = await User.create({
       email: req.body.email,
       password: hash,
     });
-    user
-      .save()
-      .then(() => {
-        return res.status(200).json({
-          result: 1,
-          data: {
-            message: "User Added Successfully!",
-          },
-        });
-      })
-      .catch((error) => {
-        return res.status(500).json({
-          result: 0,
-          error: error,
-        });
-      });
-  });
+
+    return res.status(200).json({
+      result: 1,
+      data: {
+        message: "User Added Successfully!",
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
